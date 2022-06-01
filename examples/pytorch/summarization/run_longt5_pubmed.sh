ba4fb3848045509d0875b0cec8ba5fa0662c50b1
daniel.stancl <daniel.stancl@gmail.com>
2022-06-01 22:06:11 +0200

    Some minor changes

--- examples/pytorch/summarization/run_longt5_pubmed.sh
+++ examples/pytorch/summarization/run_longt5_pubmed.sh
@@ -7,13 +7,14 @@ echo "Debug mode is $DEBUG_CHECK."
 MODEL=$([[ $DEBUG -eq 1 ]] && echo "Stancld/LongT5-Local-Base" || echo "Stancld/LongT5-TGlobal-Large")
 
 
-LAUNCH=$([[ $DEBUG -eq 1 ]] && echo "" || echo "-m torch.distributed.launch --nproc_per_node=$N_GPU")
-BF16=$([[ $DEBUG -eq 1 ]] && echo "False" || echo "True")
+LAUNCH=$([[ $DEBUG -eq 1 ]] && echo "" || echo "-m torch.distributed.launch --nproc_per_node=$N_GPU")  # 2 GPUs given
+BF16=$([[ $DEBUG -eq 1 ]] && echo "False" || echo "True")  # BF16 can be used on A100
 
 
-TOTAL_BS=128
-BS=1
-ACC_STEP=$((TOTAL_BS / BS))
+TOTAL_BATHC_SIZE=$([[ $DEBUG -eq 1 ]] && echo 1 || echo 128)
+PER_DEVICE_BATCH_SIZE=1
+ACC_STEP=$((TOTAL_BATHC_SIZE / PER_DEVICE_BATCH_SIZE))
+ACC_STEP=$([[ $DEBUG -eq 1 ]] && echo $ACC_STEP || $((ACC_STEP / N_GPU)))
 
 
 SCRIPT="python $LAUNCH run_summarization.py \
@@ -22,10 +23,10 @@ SCRIPT="python $LAUNCH run_summarization.py \
     --do_eval \
     --do_predict \
     --dataset_name ccdv/pubmed-summarization \
-    --source_prefix summarize \
+    --source_prefix 'summarize: ' \
     --max_source_length 16384 \
     --max_target_length 512 \
-    --per_device_train_batch_size $BS \
+    --per_device_train_batch_size $PER_DEVICE_BATCH_SIZE \
     --gradient_accumulation_steps $ACC_STEP \
     --optim adafactor \
     --learning_rate 0.001 \
