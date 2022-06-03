6b884cdd484bc59dce20a7130d8a3856f25af33b
daniel.stancl <daniel.stancl@gmail.com>
2022-06-03 22:27:12 +0200

    Fix device handling

--- src/transformers/models/longt5/modeling_longt5.py
+++ src/transformers/models/longt5/modeling_longt5.py
@@ -190,7 +190,9 @@ def _make_global_fixed_block_ids(
     if num_globals > 0:
         _sequence_block_ids_max = torch.max(global_block_ids, dim=-1).values.repeat(num_globals, 1).transpose(0, 1)
     else:
-        _sequence_block_ids_max = torch.zeros(batch_size, 0, dtype=global_block_ids.dtype)
+        _sequence_block_ids_max = torch.zeros(
+            batch_size, 0, dtype=global_block_ids.dtype, device=global_block_ids.device
+        )
     global_segment_ids = torch.cumsum(torch.ones(batch_size, num_globals), dim=-1) - 1
     global_segment_ids = global_segment_ids.to(attention_mask.device)
     global_segment_ids = torch.where(global_segment_ids <= _sequence_block_ids_max, 1, 0)
